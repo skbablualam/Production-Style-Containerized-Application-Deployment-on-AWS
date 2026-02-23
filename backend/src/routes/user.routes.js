d2f2c7b4f5619871827258ed4886cd89843e90bc
Bablu Alam <52425424+skbablualam@users.noreply.github.com>
2026-02-23 15:12:40 +0530

    Update user.routes.js

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -4,5 +4,8 @@ const controller = require('../controllers/user.controller');
 
 router.post('/', controller.createUser);
 router.get('/', controller.getUsers);
+router.get('/:id', controller.getUserById);
+router.put('/:id', controller.updateUser);
+router.delete('/:id', controller.deleteUser);
 
 module.exports = router;
